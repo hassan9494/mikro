9e46ac5ba40ee5380a9990fdc85f342a833927ab
hassan9494 <hassanalwan36@gmail.com>
2025-01-02 20:33:23 +0300

    fix tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,38 +10,60 @@ import React from 'react';
 import { ServerStyleSheet } from 'styled-components';
 import theme from 'theme';
 
-export default class CustomDocument extends Document {
-    static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+interface CustomDocumentInitialProps extends DocumentInitialProps {
+    headScripts: string;
+    bodyScripts: string;
+}
+
+export default class CustomDocument extends Document<CustomDocumentInitialProps> {
+    static async getInitialProps(ctx: DocumentContext): Promise<CustomDocumentInitialProps> {
         const sheet = new ServerStyleSheet();
         const originalRenderPage = ctx.renderPage;
 
-        ctx.renderPage = () =>
-            originalRenderPage({
-                enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
-            });
+        const url = process.env.NEXT_PUBLIC_REST_API_ENDPOINT;
+        // Fetch the scripts from the API
+        const res = await fetch(`${url}/tag`);
+        const data = await res.json();
 
-        const initialProps = await Document.getInitialProps(ctx);
-        return {
-            ...initialProps,
-            styles: [
-                ...React.Children.toArray(initialProps.styles),
-                ...React.Children.toArray(sheet.getStyleElement())
-            ],
-        };
+        const headScripts = data.data.filter((item: any) => item.type === 'HEAD').map((item: any) => item.script).join('');
+        const bodyScripts = data.data.filter((item: any) => item.type === 'BODY').map((item: any) => item.script).join('');
+
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App) => (props: any) =>
+                        sheet.collectStyles(<App {...props} />),
+                });
+
+            const initialProps = await Document.getInitialProps(ctx);
+            return {
+                ...initialProps,
+                styles: [
+                    ...React.Children.toArray(initialProps.styles),
+                    ...React.Children.toArray(sheet.getStyleElement())
+                ],
+                headScripts,
+                bodyScripts,
+            };
+        } finally {
+            sheet.seal();
+        }
     }
 
     render() {
         return (
-            <Html lang="en">
+            <Html lang="en" data-head-scripts={this.props.headScripts} data-body-scripts={this.props.bodyScripts}>
                 <Head>
-                    <link rel="icon" href="/favicon.ico" />
-                    <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+
                     <meta name="theme-color" content={theme.palette.primary.main} />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;700;900&display=swap" rel="stylesheet" />
+                    <script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=611a67ca030dfe001340392c&product=sticky-share-buttons' async={true}/>
                 </Head>
                 <body>
+                {/*<div className="sharethis-sticky-share-buttons"></div>*/}
                 <Main />
+                {/*<div id="modal-root" />*/}
                 <NextScript />
                 </body>
             </Html>
