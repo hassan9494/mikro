57e09ddc9f0f56563c9d56aa6c13de58f293a168
hassan9494 <hassanalwan36@gmail.com>
2026-02-23 00:17:05 +0300

    replace slug with sku

--- src/components/product-details/product-details-one/product-details-one.tsx
+++ src/components/product-details/product-details-one/product-details-one.tsx
@@ -156,7 +156,7 @@ function TabPanel(props: TabPanelProps) {
         >
             {value === index && (
                 <Box p={3}>
-                    <Typography variant="div">{children}</Typography>
+                    <Typography variant="body1">{children}</Typography>
                 </Box>
             )}
         </div>
